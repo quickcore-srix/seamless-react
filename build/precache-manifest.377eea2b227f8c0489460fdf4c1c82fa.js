f5a7d44bb2a4080ef3ca85651c78962021b02514
JAYAPAL REDDY MYAKA <jayapalreddymjpal@gmail.com>
2019-03-22 00:06:13 +0530

    dev environment added in firebase

---
+++ build/precache-manifest.377eea2b227f8c0489460fdf4c1c82fa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "02a02bbd233fe55f2baa",
+    "url": "/static/js/main.f73b8bfc.chunk.js"
+  },
+  {
+    "revision": "b4ae490ba870fb435328",
+    "url": "/static/js/2.eb79a78a.chunk.js"
+  },
+  {
+    "revision": "02a02bbd233fe55f2baa",
+    "url": "/static/css/main.39daf68f.chunk.css"
+  },
+  {
+    "revision": "1a87f18a9ffa41e20eeb46a86b2542b1",
+    "url": "/index.html"
+  }
+];
